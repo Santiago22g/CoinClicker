6ba4e86f122b584408d027c52fe76cd4bfcb5ce3
santiago22 <tasiomartin33@gmail.com>
2026-03-05 10:58:37 +0100

    shine per click added

--- Scripts/texture_button.gd
+++ Scripts/texture_button.gd
@@ -8,3 +8,6 @@ func _on_pressed():
 	
 	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.1)
 	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
+	
+	self.modulate = Color(1.089, 1.027, 0.807, 1.0)
+	tween.finished.connect(func(): self.modulate = Color(1.0, 1.0, 1.0, 1.0))
