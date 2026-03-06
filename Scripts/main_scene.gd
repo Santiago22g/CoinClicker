03498f48a03bf32100a96db870bdf99d3bae51de
santiago22 <tasiomartin33@gmail.com>
2026-03-06 10:43:58 +0100

    json buttons

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -5,6 +5,8 @@ var amount_per_click = 1
 var multiplier = 1
 var is_frenzy_active = false
 var drain_speed = 15.0 
+var upgrades_data = []
+var auto_coins_per_sec = 0
 
 @onready var click_sound = $CoinSound
 @onready var coin_meter = $TextureProgressBar 
@@ -17,7 +19,8 @@ signal coin_clicked
 
 func _ready() -> void:
 	bg_panel.visible = false
-
+	load_upgrades()
+	
 func _process(delta: float) -> void:
 	if not is_frenzy_active and coin_meter.value > 0:
 		coin_meter.value -= drain_speed * delta
@@ -46,6 +49,33 @@ func activate_frenzy() -> void:
 	coin_meter.value = 0
 	coin_meter.tint_progress = Color(1, 1, 1)
 
+func load_upgrades():
+	var file = FileAccess.open("res://upgrades.json", FileAccess.READ)
+	var json_string = file.get_as_text()
+	var json = JSON.new()
+	json.parse(json_string)
+	upgrades_data = json.data["upgrades"]
+
+func buy_upgrade(upgrade_id: String):
+	for item in upgrades_data:
+		if item["id"] == upgrade_id:
+			var actual_price = int(item["base_cost"] * pow(1.15, item["level"]))
+			if coins >= actual_price:
+				coins -= actual_price
+				item["level"] += 1
+				if item["type"] == "click":
+					amount_per_click += item["power"]
+				else:
+					auto_coins_per_sec += item["power"]
+				
+				_update_ui()
+			else:
+				print("¡No enough money!")
+
+func _update_ui() -> void:
+	total_label.text = str(coins) + " Coins"
+	emit_signal("coins_changed", coins)
+
 func _on_setting_button_pressed() -> void:
 	bg_panel.visible = !bg_panel.visible
 
@@ -64,3 +94,16 @@ func _on_bg_3_pressed() -> void:
 func _on_bg_4_pressed() -> void:
 	main_background.texture = $Panel/HBoxContainer/BG4.texture_normal
 	bg_panel.visible = false
+
+
+func _on_timer_timeout() -> void:
+	coins += auto_coins_per_sec
+	_update_ui()
+
+
+func _on_button_upgrade_1_pressed(extra_arg_0: String) -> void:
+	buy_upgrade(extra_arg_0)
+
+
+func _on_button_upgrade_2_pressed(extra_arg_0: String) -> void:
+	buy_upgrade(extra_arg_0)
